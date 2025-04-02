0d8604e099df890209efb2322f171060883f2341
wichadak <duangdao@gmail.com>
2025-04-02 11:56:26 +0700

    fix monthOfTheYear again

--- src/app.test.js
+++ src/app.test.js
@@ -7,6 +7,6 @@ test('getDay returns the long-format day of the week', () => {
     expect( day ).toBe('Wednesday');
 });
 test('getDay returns the month of the year', () => {
-    const month = monthOfTheWeek( new Date('3/11/2020') );
+    const month = monthOfTheYear( new Date('3/11/2020') );
     expect( month ).toBe('November');
 });
